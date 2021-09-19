13ae1c8da181981f662cd27d91618bd82c81ccd4
Kazukafka <pasuwaado135@gmail.com>
2021-09-19 21:36:31 +0900

    ProfileEditFunc

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 50 }
+
+  def update_without_current_password(params, *options)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { registrations: 'registrations' }
   root 'pages#home'
 
   get '/users/:id', to: 'users#show', as: 'user'
